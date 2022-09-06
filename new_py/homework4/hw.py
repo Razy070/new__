61219b1d98c98b6b4375359355654e0196055805
Razy070 <rdimovin@mail.ru>
2022-09-07 01:51:28 +0700

    up

--- new_py/homework4/hw.py
+++ new_py/homework4/hw.py
@@ -1,3 +1,49 @@
+import time
+from pprint import pprint
+import sys
+
+request_keys_default = ["url", "method", "data", "timeout", "headers"]
+methods_default = ["GET", "POST", "PUT", "DELETE", "AUTH", "HEAD", "PATCH"]
+
+
+def processing_error_auth(func):
+    def excepted(request_data, time_sleep=0.5, check_request_keys=True, check_methods=True,
+                 request_keys=request_keys_default, methods=methods_default):
+
+        if len(request_data) == 0:
+            print("Возникла ошибка")
+            time.sleep(.2)
+            raise KeyError('Подан пустой словарь')
+
+        if check_request_keys:
+            if len(request_data) < 5:
+                print("Отсутсвуют элементы с ключем:")
+                for i in request_keys:
+                    if request_data.get(i, None):
+                        pass
+                    else:
+                        print(i)
+        else:
+            print("Проверка ключа отключена")
+
+        if check_methods:
+            try:
+                if request_data["method"] not in methods:
+                    request_data_except = request_data["method"]
+                    raise NonExistentMethod(error=request_data_except)
+            except KeyError:
+                pass
+        else:
+            print("Проверка метода отключена")
+
+        result = func(request_data)
+
+        return result
+
+    return excepted
+
+
+###########################################################################
 request_from_user1 = {
     "url": "localhost/home/",
     "method": "GET",
@@ -43,5 +89,12 @@ def check_user_request():
 
 print(check_user_request())
 
-######################################################################################################################
-######################################################################################################################
+
+################################################################################
+
+class NonExistentMethod(Exception):
+
+    def __init__(self, error, ):
+        self.message = f"Метод '{error}' не существует"
+
+        super().__init__(self.message)
